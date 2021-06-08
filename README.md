2c162d14b335f3a22d309f4d581ef78ac8c66321
chilot <https://github.com/Raigyo>
2021-06-08 19:06:45 +0200

    fetch api rest nyt then display cards

--- README.md
+++ README.md
@@ -1,11 +1,8 @@
 # React and Redux: fundamental and advanced
 
-*June 2021*
+_June 2021_
 
 > 🔨 From udemy '[Reactv16 & intégration Redux : fondamentaux et avancé - Sandy Ludosky](https://www.udemy.com/course/reactv16-integration-redux-fondamentaux-et-avance)'.
-
-
-
 
 <h1 align="center">
     <img src="_readme-img/logo.png">
@@ -23,11 +20,9 @@ It helps you write applications that behave consistently, run in different envir
     <img src="_readme-img/redux-schema-01.png">
 </h1>
 
-
-
-
 ## Useful links
 
 - [Getting Started with Redux](https://redux.js.org/introduction/getting-started)
 - [Developing modern offline apps with ReactJS, Redux and Electron – Part 3 – ReactJS + Redux](https://blog.codecentric.de/en/2017/12/developing-modern-offline-apps-reactjs-redux-electron-part-3-reactjs-redux-basics/)
 - [The New York Times Developer Network](https://developer.nytimes.com/apis)
+- [Build fast, responsive sites with Bootstrap](https://getbootstrap.com/)
--- src/App.js
+++ src/App.js
@@ -1,23 +1,101 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+const placeholderImg =
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQDsfRCwQvpsd4O5b6IK9evG9H1PTxZLoI6ew5iVnlz3ftQjMBQ";
+
+const Card = ({ id, published_date, title, abstract, section, url, media }) => {
+  const image = (media) => {
+    return media ? media[0]["media-metadata"][0].url : placeholderImg;
+  };
+
+  return (
+    <div className="card d-flex flex-row justify-content-between">
+      <div className="d-flex">
+        <img
+          className="mr-4 rounded"
+          src={image(media)}
+          alt="placeholder"
+          width="170"
+          height="170"
+        />
+        <div className="d-flex flex-column" style={{ width: "75%" }}>
+          <p>
+            <small>
+              <strong>{published_date}</strong>
+            </small>
+          </p>
+          <h5>
+            <a
+              href={url}
+              target="_blank"
+              rel="noreferrer"
+              style={{ color: "#2980b9" }}
+            >
+              {title}
+            </a>
+          </h5>
+          <i>{abstract}</i>
+        </div>
+      </div>
+      <p className="d-flex text-right" style={{ color: "#2980b9" }}>
+        <strong>{section}</strong>
+      </p>
+    </div>
+  );
+};
+
+const Navigation = () => (
+  <ul className="nav nav-pills">
+    <li className="nav-item">
+      <a className="nav-link active" aria-current="page" href="#0">
+        Top stories
+      </a>
+    </li>
+    <li className="nav-item">
+      <a className="nav-link" href="#0">
+        Most Popular
+      </a>
+    </li>
+  </ul>
+);
 
 function App() {
+  // getter, setter = Array hooks
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      "https://api.nytimes.com/svc/mostpopular/v2/emailed/7.json?api-key=Zx9nn2HMNuqg0B3x0OvpGsQt6hdNv1Be"
+    )
+      .then((response) => {
+        // console.log(response);
+        // debugger;
+        if (response.ok) {
+          return response.json();
+        }
+      })
+      .then((data) => {
+        // console.log(data.results)
+        setResults(data.results);
+      });
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="col-md-6 offset-3" style={{ marginTop: "20px" }}>
+        <Navigation />
+        <br />
+        {results.map((result) => {
+          return (
+            <>
+              <Card {...result} /> <br />
+            </>
+          );
+        })}
+        {/* {results.map(() => (
+          <Card />
+        ))} */}
+      </div>
     </div>
   );
 }
